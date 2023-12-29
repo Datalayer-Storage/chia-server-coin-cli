abdc19a6c934de9910055b163e8754fa48ec9667
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-12-29 09:58:05 -0500

    chore: rebuild project

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.constants = void 0;
-exports.constants = {
-    defaultCoinAmountInMojo: 300000000,
-    defaultFeeAmountInMojo: 300000000,
-};
